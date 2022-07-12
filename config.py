08927ffad01ab2fe3f08c98a4efa7f1e41d16d1c
Maaz <zmzeromz@gmail.com>
2022-07-12 19:11:38 +0200

    environ fix

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 from ccxt import binance
 
-API_KEY = os.environ.API_KEY
-API_SECRET = os.environ.env.API_SECRET
-PASSPHRASE = os.environ.env.PASSPHRASE
+API_KEY = os.environ['API_KEY']
+API_SECRET = os.environ['API_SECRET']
+PASSPHRASE = os.environ['PASSPHRASE']
 
 exchange = binance({
    "apiKey": API_KEY, 
