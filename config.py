4e3444c288a95ee2608747f46aa301e8c10cd115
Maaz <zmzeromz@gmail.com>
2022-07-12 18:58:20 +0200

    fixed typo

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 from ccxt import binance
 
-API_KEY = os.environ.APIKEY
-API_SECRET = os.environ.env.APISECRET
+API_KEY = os.environ.API_KEY
+API_SECRET = os.environ.env.API_SECRET
 PASSPHRASE = os.environ.env.PASSPHRASE
 
 exchange = binance({
