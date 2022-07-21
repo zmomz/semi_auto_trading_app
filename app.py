02c76517d432030215de160c9d0f8bb4b48d4e01
Maaz <zmzeromz@gmail.com>
2022-07-21 19:58:40 +0200

    activating real api

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from flask_migrate import Migrate
 import config
 # exchange = config.
 from config import exchange
-exchange.set_sandbox_mode(True)
+# exchange.set_sandbox_mode(True)
 ## Init app
 
 app = Flask(__name__)
@@ -83,40 +83,40 @@ def verify_password(username_or_token, password):
     g.user = user
     return True
 
-@app.route('/api/users', methods=['GET'])
-def register_user():
-    return render_template('register.html')
+# @app.route('/api/users', methods=['GET'])
+# def register_user():
+#     return render_template('register.html')
 
 
-@app.route('/api/users', methods=['POST'])
-def new_user():
-    username = request.json.get('username')
-    password = request.json.get('password')
-    if username is None or password is None:
-        abort(400)    # missing arguments
-    if User.query.filter_by(username=username).first() is not None:
-        abort(400)    # existing user
-    user = User(username=username)
-    user.hash_password(password)
-    db.session.add(user)
-    db.session.commit()
-    return (jsonify({'username': user.username}), 201,
-            {'Location': url_for('get_user', id=user.id, _external=True)})
+# @app.route('/api/users', methods=['POST'])
+# def new_user():
+#     username = request.json.get('username')
+#     password = request.json.get('password')
+#     if username is None or password is None:
+#         abort(400)    # missing arguments
+#     if User.query.filter_by(username=username).first() is not None:
+#         abort(400)    # existing user
+#     user = User(username=username)
+#     user.hash_password(password)
+#     db.session.add(user)
+#     db.session.commit()
+#     return (jsonify({'username': user.username}), 201,
+#             {'Location': url_for('get_user', id=user.id, _external=True)})
 
 
-@app.route('/api/users/<int:id>')
-def get_user(id):
-    user = User.query.get(id)
-    if not user:
-        abort(400)
-    return jsonify({'username': user.username})
+# @app.route('/api/users/<int:id>')
+# def get_user(id):
+#     user = User.query.get(id)
+#     if not user:
+#         abort(400)
+#     return jsonify({'username': user.username})
 
 
-@app.route('/api/token')
-@auth.login_required
-def get_auth_token():
-    token = g.user.generate_auth_token(600)
-    return jsonify({'token': token.decode('ascii'), 'duration': 600})
+# @app.route('/api/token')
+# @auth.login_required
+# def get_auth_token():
+#     token = g.user.generate_auth_token(600)
+#     return jsonify({'token': token.decode('ascii'), 'duration': 600})
 
 
 @app.route('/api/resource')
